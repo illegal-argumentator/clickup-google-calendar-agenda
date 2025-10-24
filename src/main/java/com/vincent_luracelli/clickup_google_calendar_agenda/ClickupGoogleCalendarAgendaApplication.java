abb52e5c25da0594da52b3c3c4e4e181b7a700d8
HaaaamBurger <gemasclashes@gmail.com>
2025-10-24 15:30:03 +0300

    init project

---
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/ClickupGoogleCalendarAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.vincent_luracelli.clickup_google_calendar_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickupGoogleCalendarAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickupGoogleCalendarAgendaApplication.class, args);
+	}
+
+}
