f4edba85343bc9a694337c5bc8e4f415c5e61c1b
HaaaamBurger <gemasclashes@gmail.com>
2025-11-11 13:44:06 +0200

    added Patch to cors

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/common/config/WebConfig.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/common/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://dashboard.dataox.io", "http://localhost:3520")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
