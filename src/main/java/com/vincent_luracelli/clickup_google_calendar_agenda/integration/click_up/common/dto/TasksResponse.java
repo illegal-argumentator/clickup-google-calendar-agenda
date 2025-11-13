29492f10cc886890f2a67d2ed0e11092b34be93e
HaaaamBurger <gemasclashes@gmail.com>
2025-11-13 13:00:37 +0200

    tasks response

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/TasksResponse.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/TasksResponse.java
@@ -1,15 +1,15 @@
 package com.vincent_luracelli.clickup_google_calendar_agenda.integration.click_up.common.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.vincent_luracelli.clickup_google_calendar_agenda.integration.click_up.common.dto.embedded.Task;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TasksResponse {
 
         private List<Task> tasks;
-
-        @JsonProperty("last_page")
-        private boolean lastPage;
 
 }
