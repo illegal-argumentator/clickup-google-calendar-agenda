11afdf966f5097dedb1454e89b44f5043e957306
HaaaamBurger <gemasclashes@gmail.com>
2025-11-13 13:05:39 +0200

    EventListResponse

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/google_calendar/common/dto/EventListResponse.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/google_calendar/common/dto/EventListResponse.java
@@ -1,15 +1,16 @@
 package com.vincent_luracelli.clickup_google_calendar_agenda.integration.google_calendar.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class EventListResponse {
-        private List<EventResponse> items;
-        private String nextTokenPage;
-}
+public class EventListResponse { private List<EventResponse> items;}
