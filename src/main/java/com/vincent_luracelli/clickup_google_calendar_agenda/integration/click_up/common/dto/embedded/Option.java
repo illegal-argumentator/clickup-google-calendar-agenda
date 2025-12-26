6601f5049bf5ab743726cdc25a8fe851f36e768a
HaaaamBurger <gemasclashes@gmail.com>
2025-12-26 11:42:37 +0200

    Added label to option

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/embedded/Option.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/embedded/Option.java
@@ -6,5 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record Option(
         String id,
         String name,
-        String color) {
+        String color,
+        String label) {
 }
