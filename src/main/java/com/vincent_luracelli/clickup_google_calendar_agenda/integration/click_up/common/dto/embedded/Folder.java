4181f4c53210b5f9e9fa855a8b82c6e8cbaa403b
Vlad <laft921@gmail.com>
2026-01-13 12:44:10 +0200

    Add list property to Folder record and define nested FolderList record

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/embedded/Folder.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/integration/click_up/common/dto/embedded/Folder.java
@@ -2,8 +2,19 @@ package com.vincent_luracelli.clickup_google_calendar_agenda.integration.click_u
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Folder(
         String id,
-        String name) {
+        String name,
+        List<FolderList> list
+
+) {
+
+    record FolderList(
+            String id,
+            String name
+    ) {
+    }
 }
