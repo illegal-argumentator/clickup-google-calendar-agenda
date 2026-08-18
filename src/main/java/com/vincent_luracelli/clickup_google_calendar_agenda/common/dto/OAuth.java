690d185b1aebe9aa1cfd9d572022ddb785ee36a2
HaaaamBurger <gemasclashes@gmail.com>
2026-08-18 10:04:39 +0300

    props added

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/common/dto/OAuth.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/common/dto/OAuth.java
@@ -1,16 +1,13 @@
 package com.vincent_luracelli.clickup_google_calendar_agenda.common.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Data
 public class OAuth {
 
     private String clientId;
-
     private String clientSecret;
 
 }
