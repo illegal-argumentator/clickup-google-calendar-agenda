90f110a9282e122d0c9d508cce60777603215338
HaaaamBurger <gemasclashes@gmail.com>
2026-09-24 08:36:15 +0200

    refactoring

--- src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/schedulers/ClickUpWebHookHealthcheckScheduler.java
+++ src/main/java/com/vincent_luracelli/clickup_google_calendar_agenda/schedulers/ClickUpWebHookHealthcheckScheduler.java
@@ -2,11 +2,9 @@ package com.vincent_luracelli.clickup_google_calendar_agenda.schedulers;
 
 import com.vincent_luracelli.clickup_google_calendar_agenda.service.WebhookHealthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClickUpWebHookHealthcheckScheduler {
@@ -15,7 +13,6 @@ public class ClickUpWebHookHealthcheckScheduler {
 
     @Scheduled(fixedDelay = 60 * 1000, initialDelay = 60 * 1000)
     private void runScheduler() {
-        log.info("Processing webhook healthcheck.");
         healthService.checkAndFix();
     }
 
